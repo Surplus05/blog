86ae760f9b87d0f53af633a2c23e457141176baa
st.kim <st.kim@eigene.ai>
2024-06-04 02:16:18 +0900

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,39 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  author: "Surplus",
+  desc: "Surplus's Blog.",
+  title: "Surplus.Dev",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 6,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+};
+
+export const LOCALE = {
+  lang: "kr", // html lang code. Set this empty and default will be "en"
+  langTag: ["kr-KR"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/Surplus05",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:sungtae1005@gmail.com",
+    linkTitle: ``,
+    active: true,
+  },
+];
---
+++ src/utils/generateOgImages.tsx
@@ -0,0 +1,53 @@
+import satori, { type SatoriOptions } from "satori";
+import { Resvg } from "@resvg/resvg-js";
+import { type CollectionEntry } from "astro:content";
+import postOgImage from "./og-templates/post";
+import siteOgImage from "./og-templates/site";
+
+const fetchFonts = async () => {
+  // Regular Font
+  const fontFileRegular = await fetch(
+    "https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff"
+  );
+  const fontRegular: ArrayBuffer = await fontFileRegular.arrayBuffer();
+
+  return { fontRegular };
+};
+
+const { fontRegular } = await fetchFonts();
+
+const options: SatoriOptions = {
+  width: 1200,
+  height: 630,
+  embedFont: true,
+  fonts: [
+    {
+      name: "Pretendard",
+      data: fontRegular,
+      weight: 400,
+      style: "normal",
+    },
+    {
+      name: "Pretendard",
+      data: fontRegular,
+      weight: 600,
+      style: "normal",
+    },
+  ],
+};
+
+function svgBufferToPngBuffer(svg: string) {
+  const resvg = new Resvg(svg);
+  const pngData = resvg.render();
+  return pngData.asPng();
+}
+
+export async function generateOgImageForPost(post: CollectionEntry<"blog">) {
+  const svg = await satori(postOgImage(post), options);
+  return svgBufferToPngBuffer(svg);
+}
+
+export async function generateOgImageForSite() {
+  const svg = await satori(siteOgImage(), options);
+  return svgBufferToPngBuffer(svg);
+}
